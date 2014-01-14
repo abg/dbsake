6702abcf9a78fa3b809816a2743c2ee2c1a2860f
Andrew Garner <muzazzi@gmail.com>
2014-01-14 20:32:00 +0000

    add mysql-sandbox command to dbsake

---
+++ dbsake/sandbox/__init__.py
@@ -0,0 +1,85 @@
+"""
+dbsake.sandbox
+~~~~~~~~~~~~~~
+
+MySQL temporary instance  support
+
+"""
+from __future__ import print_function
+
+import os
+import sys
+import time
+
+from dbsake import baker
+
+from . import util
+
+@baker.command(name='mysql-sandbox',
+               shortopts=dict(sandbox_directory="d"))
+def mysql_sandbox(sandbox_directory=None):
+    mysqld_safe = util.which("mysqld_safe")
+    mysql = util.which("mysql")
+    mysqld_search_path = ['/usr/libexec', '/usr/sbin', os.environ['PATH'] ]
+    mysqld = util.which("mysqld", path=os.pathsep.join(mysqld_search_path))
+
+    if not mysqld:
+        print("[ERROR] Failed to find mysqld in environment PATH",
+              file=sys.stderr)
+        return 1
+
+    if not mysqld_safe:
+        print("[ERROR] Failed to find mysqld_safe in environment PATH",
+              file=sys.stderr)
+        return 1
+
+    if not mysql:
+        # /usr/bin/mysql missing is not fatal, but probably a serious issue
+        print("[WARNING] Failed to find mysql commandline client in path", file=sys.stderr)
+
+    if not sandbox_directory:
+        tag = time.strftime("%Y%m%d_%H%M%S", time.gmtime())
+        sandbox_directory = os.path.expanduser('~/sandboxes/sandbox_' + tag)
+
+    if os.path.isdir(sandbox_directory) and len(os.listdir(sandbox_directory)):
+        print("[ERROR] %s exists and is not empty. Aborting." % sandbox_directory,
+              file=sys.stderr)
+        return 2
+
+    os.makedirs(sandbox_directory)
+    print("Created %s" % sandbox_directory)
+    datadir = os.path.join(sandbox_directory, 'data')
+    os.makedirs(datadir, 0770)
+    print("Created %s" % datadir)
+    tmpdir = os.path.join(sandbox_directory, 'tmp')
+    os.makedirs(tmpdir, 0770)
+    print("Created %s" % tmpdir)
+
+    print("Generating random password for root@localhost...")
+    password = util.mkpassword(length=17)
+
+    print("Generating my.sandbox.cnf...")
+    defaults = util.generate_defaults(sandbox_directory, user='root', password=password)
+    print("Bootstrapping new mysql instance (mysqld=%s datadir=%s)" % (mysqld, datadir))
+    bootstrap_log = os.path.join(sandbox_directory, 'bootstrap.log')
+    print("  For details see %s" % bootstrap_log)
+    try:
+        util.bootstrap_mysqld(mysqld=mysqld,
+                              defaults_file=defaults,
+                              logfile=bootstrap_log,
+                              content=util.mysql_install_db(password))
+    except IOError:
+        print("[ERROR] Bootstrap process failed.  See %s" % bootstrap_log)
+        return 1
+    print("Bootstrapping complete!")
+
+    initscript = os.path.join(sandbox_directory, 'sandbox.sh')
+    print("Generating init script %s..." % initscript)
+    util.generate_initscript(sandbox_directory, mysqld_safe=mysqld_safe, mysql=mysql)
+
+    print("Sandbox creation complete!")
+    print("You may start your sandbox by running: %s start" % initscript)
+    print("You may login to your sandbox by running: %s shell" % initscript)
+    print("   or by running: %s --socket=%s" % (mysql, os.path.join(datadir, 'mysql.sock')))
+    print("Credentials are stored in %s" % defaults)
+    return 0
---
+++ dbsake/sandbox/util.py
@@ -0,0 +1,148 @@
+import codecs
+import os
+import pkgutil
+import random
+import stat
+import string
+import sys
+
+from dbsake import tempita
+from dbsake import sarge
+
+def which(cmd, mode=os.F_OK | os.X_OK, path=None):
+    """Given a command, mode, and a PATH string, return the path which
+    conforms to the given mode on the PATH, or None if there is no such
+    file.
+
+    `mode` defaults to os.F_OK | os.X_OK. `path` defaults to the result
+    of os.environ.get("PATH"), or can be overridden with a custom search
+    path.
+
+    """
+    # Check that a given file can be accessed with the correct mode.
+    # Additionally check that `file` is not a directory, as on Windows
+    # directories pass the os.access check.
+    def _access_check(fn, mode):
+        return (os.path.exists(fn) and os.access(fn, mode)
+                and not os.path.isdir(fn))
+
+    # If we're given a path with a directory part, look it up directly rather
+    # than referring to PATH directories. This includes checking relative to the
+    # current directory, e.g. ./script
+    if os.path.dirname(cmd):
+        if _access_check(cmd, mode):
+            return cmd
+        return None
+
+    if path is None:
+        path = os.environ.get("PATH", os.defpath)
+    if not path:
+        return None
+    path = path.split(os.pathsep)
+
+    if sys.platform == "win32":
+        # The current directory takes precedence on Windows.
+        if not os.curdir in path:
+            path.insert(0, os.curdir)
+
+        # PATHEXT is necessary to check on Windows.
+        pathext = os.environ.get("PATHEXT", "").split(os.pathsep)
+        # See if the given file matches any of the expected path extensions.
+        # This will allow us to short circuit when given "python.exe".
+        # If it does match, only test that one, otherwise we have to try
+        # others.
+        if any(cmd.lower().endswith(ext.lower()) for ext in pathext):
+            files = [cmd]
+        else:
+            files = [cmd + ext for ext in pathext]
+    else:
+        # On other platforms you don't have things like PATHEXT to tell you
+        # what file suffixes are executable, so just pass on cmd as-is.
+        files = [cmd]
+
+    seen = set()
+    for dir in path:
+        normdir = os.path.normcase(dir)
+        if not normdir in seen:
+            seen.add(normdir)
+            for thefile in files:
+                name = os.path.join(dir, thefile)
+                if _access_check(name, mode):
+                    return name
+    return None
+
+def escape(value):
+    return value.replace('"', '\\"').replace("'", "\\'").replace("\\", "\\\\")
+
+def load_template(name):
+    pkg = __name__.rpartition('.')[0]
+    data = pkgutil.get_data(pkg, '/'.join(['templates', name]))
+    return tempita.Template(data.decode("utf8"), namespace={'escape': escape})
+ 
+def render_template(name, **kwargs):
+    return load_template(name).substitute(**kwargs)
+  
+def mkpassword(length=8):
+    alphabet = string.letters + string.digits + string.punctuation
+    return ''.join(random.sample(alphabet, length))
+
+def generate_defaults(path, user, password):
+    # need to read a template to do this right
+    template = load_template("my.sandbox.cnf")
+
+    content = template.substitute(
+        sandbox_root=path,
+        mysql_user=os.environ['USER'],
+        user=user,
+        password=password,
+        datadir=os.path.join(path, 'data'),
+        socket=os.path.join(path, 'data', 'mysql.sock'),
+        networking=False,
+        additional_options=(),
+        open_files_limit=1024,
+    )
+
+    options_path = os.path.join(path, 'my.sandbox.cnf')
+    with codecs.open(options_path, 'w', encoding='utf8') as fileobj:
+        fileobj.write(content)
+
+    return options_path
+
+def cat(path):
+    with codecs.open(path, 'r', encoding='utf8') as fileobj:
+        return fileobj.read()
+
+def mysql_install_db(password, share_path='/usr/share/mysql'):
+    join = os.path.join
+    content = [
+        render_template("bootstrap_initialize.sql"),
+        cat(join(share_path, 'mysql_system_tables.sql')),
+        cat(join(share_path, 'mysql_system_tables_data.sql')),
+        cat(join(share_path, 'fill_help_tables.sql')),
+        render_template("secure_mysql_installation.sql", password=password),
+    ]
+    return os.linesep.join(content)
+
+def bootstrap_mysqld(mysqld, defaults_file, logfile, content):
+    cmd = sarge.shell_format("{0} --defaults-file={1} --bootstrap -vvvvv",
+                             mysqld, defaults_file)
+    with open(logfile, 'wb') as stderr:
+        pipeline = sarge.run(cmd,
+                             input=content,
+                             stdout=stderr,
+                             stderr=stderr)
+        if sum(pipeline.returncodes) != 0:
+            raise RuntimeError("Bootstrap process failed")
+        print "pipeline.returncodes: %r" % (pipeline.returncodes, )
+
+def generate_initscript(sandbox_directory, **kwargs):
+    content = render_template("sandbox.sh", 
+                              sandbox_root=sandbox_directory,
+                              **kwargs)
+
+    sandbox_sh_path = os.path.join(sandbox_directory, 'sandbox.sh')
+    with codecs.open(sandbox_sh_path, 'w', encoding='utf8') as fileobj:
+        # ensure initscript is executable by current user + group
+        mode = stat.S_IRUSR|stat.S_IXUSR|stat.S_IRGRP|stat.S_IXGRP
+        os.fchmod(fileobj.fileno(), mode)
+        fileobj.write(content)
