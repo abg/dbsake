4d8a3c9d43366652308affa3f689e2756b814c9b
Andrew Garner <muzazzi@gmail.com>
2014-01-21 23:55:01 +0000

    don't purge all mysql.db users
    - now secure_mysql_installation.sql only purges the test and test_%
      db privileges.

--- dbsake/mysql/sandbox/templates/secure_mysql_installation.sql
+++ dbsake/mysql/sandbox/templates/secure_mysql_installation.sql
@@ -1,6 +1,6 @@
 -- Secure database during bootstrap by removing anonymous users
 -- and setting a password
 USE mysql;
-DELETE FROM `db`;
+DELETE FROM `db` WHERE Db = 'test' OR Db = 'test\_%';
 DELETE FROM `user` WHERE User = '';
 UPDATE `user` SET PASSWORD = PASSWORD('{{password|escape}}') WHERE User = 'root';
