52a10f1469b53b4deb54be3346a41efbbc81d6a9
Andrew Garner <muzazzi@gmail.com>
2014-07-18 06:34:39 +0000

    update setup.py based on cookiecutter pypackage template

--- setup.py
+++ setup.py
@@ -1,23 +1,61 @@
-from setuptools import setup, find_packages
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from dbsake import __version__
+import pkgutil
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+import dbsake
+
+
+def find_packages(path, prefix):
+    walk = pkgutil.walk_packages(path, prefix + '.')
+    return [prefix] + [name for _, name, ispkg in walk if ispkg]
+
+
+requirements = open('requirements.txt').read().split()
+test_requirements = open('test_requirements.txt').read().split()
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
 
 setup(
     name='dbsake',
-    version=__version__,
-    description="",
-    long_description="",
-    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-    keywords='',
-    author='',
-    author_email='',
-    url='',
-    license='',
-    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    version=dbsake.__version__,
+    description='db admin\'s (s)wiss (a)rmy (k)nif(e) for MySQL',
+    long_description=readme + '\n\n' + history,
+    author='Andrew Garner',
+    author_email='andrew.garner@rackspace.com',
+    url='https://github.com/abg/dbsake',
+    packages=find_packages(dbsake.__path__, dbsake.__name__),
+    package_dir={'dbsake':
+                 'dbsake'},
     include_package_data=True,
+    install_requires=requirements,
+    license="GPLv2",
     zip_safe=False,
-    entry_points="""
-    [console_scripts]
-    dbsake = dbsake:main
-    """,
+    keywords='dbsake',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    entry_points={
+        'console_scripts': [
+            'dbsake = dbsake.cli:main',
+        ]
+    },
 )
