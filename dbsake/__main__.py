3af8fa0c5a433d4da62fb85afe6271d53e248acd
Andrew Garner <muzazzi@gmail.com>
2014-07-23 00:29:09 +0000

    add dbsake/__main__.py
    This allows running dbsake directly from the source tree via
    python dbsake.  This does require jinja2 and click to otherwise
    be installed in the environment (either virtualenv or in
    global site packages).

---
+++ dbsake/__main__.py
@@ -0,0 +1,21 @@
+"""
+dbsake.__main__
+~~~~~~~~~~~~~~~
+
+Executable package support
+"""
+
+import sys
+
+
+def main():
+    if __package__ == '':
+        # To be able to run 'python wheel-0.9.whl/wheel':
+        import os.path
+        path = os.path.dirname(os.path.dirname(__file__))
+        sys.path[0:0] = [path]
+    import dbsake.cli
+    sys.exit(dbsake.cli.main())
+
+if __name__ == '__main__':
+    sys.exit(main())
