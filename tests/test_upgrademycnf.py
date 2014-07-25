475c9db1cb9cf10abaeee8621d707e0e3ba055be
Andrew Garner <muzazzi@gmail.com>
2014-07-25 18:42:36 +0000

    add sample my.cnf for testing upgrade-mycnf command
    - previously read /etc/my.cnf by default and so the test was
      failing
    - new test data exercises common behaviors like innodb-plugin
      rewriting (5.1 -> 5.5), set-variable fixing (from ancient my.cnf
      formats), inline-comment handling, etc.

--- tests/test_upgrademycnf.py
+++ tests/test_upgrademycnf.py
@@ -3,6 +3,8 @@ Test dbsake sandbox
 """
 from __future__ import unicode_literals
 
+import os
+
 from click.testing import CliRunner
 
 from dbsake.cli.cmd.mycnf import upgrade_mycnf
@@ -10,7 +12,9 @@ from dbsake.cli.cmd.mycnf import upgrade_mycnf
 
 def test_upgrade_mycnf_basic():
     runner = CliRunner()
+    config_path = os.path.join(os.path.dirname(__file__), 'my.cnf')
     with runner.isolated_filesystem():
-        result = runner.invoke(upgrade_mycnf, ['--patch'],
+        result = runner.invoke(upgrade_mycnf,
+                               ['--config=%s' % config_path, '--patch'],
                                auto_envvar_prefix='DBSAKE', obj={})
     assert result.exit_code == 0
