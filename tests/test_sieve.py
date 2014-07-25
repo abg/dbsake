a749adc5dee5a9cfbfe68820d10988ec9c4db93f
Andrew Garner <muzazzi@gmail.com>
2014-07-25 20:05:53 +0000

    fix python2.6 compatibility issues in test_sieve.py

--- tests/test_sieve.py
+++ tests/test_sieve.py
@@ -3,13 +3,24 @@ Test dbsake cli
 """
 from __future__ import unicode_literals
 
-import contextlib
 import os
 import gzip
 
 from click.testing import CliRunner
 
 from dbsake.cli.cmd.sieve import sieve_cli
+
+
+class _GzipFile(gzip.GzipFile):
+    """Shim for GzipFile so instances can be used as context managers"""
+    closed = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.close()
+        self.closed = True
 
 
 def test_sieve_stream():
@@ -21,7 +32,7 @@ def test_sieve_stream():
         '--exclude-table=sakila.actor_info',
         '--no-master-data',
     ]
-    with contextlib.closing(gzip.open(path, "rb")) as fileobj:
+    with _GzipFile(path, "rb") as fileobj:
         result = runner.invoke(sieve_cli, args, obj={}, input=fileobj)
     assert result.exit_code == 0
 
@@ -31,6 +42,6 @@ def test_sieve_directory():
     sakila_path = os.path.join(os.path.dirname(__file__), 'sakila.sql.gz')
     args = ['--format=directory']
     with runner.isolated_filesystem():
-        with contextlib.closing(gzip.open(sakila_path, 'rb')) as fileobj:
+        with _GzipFile(sakila_path, 'rb') as fileobj:
             result = runner.invoke(sieve_cli, args, obj={}, input=fileobj)
         assert result.exit_code == 0
