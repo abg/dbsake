d90a26a20ac60e5cc198450792ef46957407abf0
Andrew Garner <muzazzi@gmail.com>
2014-07-25 20:06:10 +0000

    force tests to be run in UTC

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,3 +4,9 @@ dbsake.tests
 
 Test suite for dbsake
 """
+
+import os
+
+# all timestamps in tst data reference UTC, so we set this here
+# XXX: this needs to be handled more cleanly
+os.environ['TZ'] = 'UTC'
