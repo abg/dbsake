536755b55443d1fadbef9d6728e8a64ab1f23d3d
Andrew Garner <muzazzi@gmail.com>
2014-08-01 21:07:33 +0000

    update sieve tests to use transparent decompression
    - removed requirement for GzipFile preprocessing in the
      test suite and exercises the decompression api.

--- tests/test_sieve.py
+++ tests/test_sieve.py
@@ -4,23 +4,10 @@ Test dbsake cli
 from __future__ import unicode_literals
 
 import os
-import gzip
 
 from click.testing import CliRunner
 
 from dbsake.cli.cmd.sieve import sieve_cli
-
-
-class _GzipFile(gzip.GzipFile):
-    """Shim for GzipFile so instances can be used as context managers"""
-    closed = False
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.close()
-        self.closed = True
 
 
 def test_sieve_stream():
@@ -31,17 +18,16 @@ def test_sieve_stream():
         '--table=sakila.actor*',
         '--exclude-table=sakila.actor_info',
         '--no-master-data',
+        '--input-file=' + path
     ]
-    with _GzipFile(path, "rb") as fileobj:
-        result = runner.invoke(sieve_cli, args, obj={}, input=fileobj)
+    result = runner.invoke(sieve_cli, args, obj={})
     assert result.exit_code == 0
 
 
 def test_sieve_directory():
     runner = CliRunner()
     sakila_path = os.path.join(os.path.dirname(__file__), 'sakila.sql.gz')
-    args = ['--format=directory']
+    args = ['--format=directory', '--input-file=' + sakila_path]
     with runner.isolated_filesystem():
-        with _GzipFile(sakila_path, 'rb') as fileobj:
-            result = runner.invoke(sieve_cli, args, obj={}, input=fileobj)
+        result = runner.invoke(sieve_cli, args, obj={})
         assert result.exit_code == 0
