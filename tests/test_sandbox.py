3164fe8a1e03a45bca5f5adbdeff7a0af7571ab7
Andrew Garner <muzazzi@gmail.com>
2017-01-30 16:28:04 +0000

    Avoid ledir in my.sandbox.cnf
    As of MySQL 5.6.35,5.5.54,5.7.17 ledir is no longer allowed as
    a [mysqld_safe] option in a config file.  This option may only
    be provided as a command line argument.
    
    Fixes #103

--- tests/test_sandbox.py
+++ tests/test_sandbox.py
@@ -50,6 +50,40 @@ def test_sandbox_50():
         assert result.exit_code == 0
 
 
+# TODO: parameterize the test_sandbox_XX tests to avoid this boiler plate
+# TODO: split these tests into separate w/ & w/o datasource
+@pytest.mark.skipif(os.environ.get('TRAVIS', 'false') == 'true',
+                    reason="Skipping sandbox CDN tests on travis-ci")
+def test_sandbox_56():
+    runner = CliRunner()
+    with runner.isolated_filesystem():
+        sbdir = os.path.abspath('test_sandbox')
+        result = runner.invoke(sandbox_cli,
+                               ['-d', sbdir, '-m', '5.6.35'],
+                               auto_envvar_prefix='DBSAKE', obj={})
+        assert result.exit_code == 0
+        result = runner.invoke(sandbox_cli,
+                               ['--force',
+                                '-d', sbdir,
+                                '-m', '5.6.35'],
+                               auto_envvar_prefix='DBSAKE', obj={})
+        assert result.exit_code == 0
+        tar = tarfile.open("backup.tar.gz", "w:gz")
+        cwd = os.getcwd()
+        os.chdir(os.path.join(sbdir, 'data'))
+        tar.add('.')
+        tar.close()
+        os.chdir(cwd)
+        result = runner.invoke(sandbox_cli,
+                               ['--force',
+                                '-d', sbdir,
+                                '-m', '5.6.35',
+                                '-s', 'backup.tar.gz',
+                                '-t', 'mysql.user'],
+                               auto_envvar_prefix='DBSAKE', obj={})
+        assert result.exit_code == 0
+
+
 '''
 def test_sandbox_51():
     runner = CliRunner()
