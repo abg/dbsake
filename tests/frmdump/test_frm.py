da19af367368f6d3570a763142b78f426f20e51e
Andrew Garner <muzazzi@gmail.com>
2017-02-14 17:34:31 +0000

    Cleanup frmdump tests
    - test_data -> fixtures
    - cleanup pytest.parameterize, so verbose output is readable
      test_frm_decode now just gets a name argument, and derives
      the .frm and expected sql output from that so pytest -v
      gives clean output like:
    
        tests/frmdump/test_frm.py::test_frm_decode[tokudb_test_5] PASSED
    
      Previously this output a full local path to the .frm with the raw
      sql string, which made the test output unreadable.

--- tests/frmdump/test_frm.py
+++ tests/frmdump/test_frm.py
@@ -14,6 +14,9 @@ from dbsake.cli.cmd.frm import frmdump
 from dbsake.core.mysql.frm import tablename
 
 
+FIXTURE_PATH = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+
 def clean_frm(data):
     return "\n".join([line for line in data.splitlines()
                      if line.strip() and not line.startswith("--")]) + "\n"
@@ -26,29 +29,29 @@ def load_frm_test_data():
     yields a path to an .frm and that .frm's expected result.  This is
     used to test that the frmdump command's result matches our expectations
     """
-    test_data = os.path.join(os.path.dirname(__file__), 'test_data', '*.frm')
+    test_data = os.path.join(FIXTURE_PATH, "*.frm")
     for frm_path in glob.glob(test_data):
-        sql_path = os.path.splitext(frm_path)[0] + '.sql'
-        with codecs.open(sql_path, 'r', 'utf-8') as fileobj:
-            expected_sql = fileobj.read()
-        yield frm_path, expected_sql
+        yield os.path.splitext(os.path.basename(frm_path))[0]
 
 
-@pytest.mark.parametrize("frm_path,expected_sql", load_frm_test_data())
-def test_frm_decode(frm_path, expected_sql):
+@pytest.mark.parametrize("name", load_frm_test_data())
+def test_frm_decode(name):
     runner = CliRunner()
+    frm_path = os.path.join(FIXTURE_PATH, name + '.frm')
+    sql_path = os.path.join(FIXTURE_PATH, name + '.sql')
     result = runner.invoke(frmdump, [frm_path], obj={})
     assert result.exit_code == 0
     derived_sql = clean_frm(result.output)
+    with codecs.open(sql_path, 'r', 'utf-8') as fileobj:
+        expected_sql = fileobj.read()
     assert derived_sql == expected_sql
 
 
 def test_frmdump_recursive():
     runner = CliRunner()
-    frm_path = os.path.join(os.path.dirname(__file__), 'test_data')
-    expected_frm_count = len([name for name in os.listdir(frm_path)
+    expected_frm_count = len([name for name in os.listdir(FIXTURE_PATH)
                               if name.endswith('.frm')])
-    result = runner.invoke(frmdump, ['--recursive', frm_path], obj={})
+    result = runner.invoke(frmdump, ['--recursive', FIXTURE_PATH], obj={})
     computed_frm_count = result.output.count('\nCREATE ')
     assert result.exit_code == 0
     assert computed_frm_count == expected_frm_count
